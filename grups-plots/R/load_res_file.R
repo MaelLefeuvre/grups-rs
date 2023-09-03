337a2b62befa9981dca3408f5661685f2363b585
MaelLefeuvre <lefeuvre.m.p@gmail.com>
2023-09-03 15:28:50 +0200

    split grups.plot into a separate project

--- grups-plots/R/load_res_file.R
+++
@@ -1,7 +0,0 @@
-#' @export
-#' @importFrom utils read.table
-#' @param path path leading to a GRUPS .result summary file
-#' @return dataframe containing simulation results for all pairs.
-load_res_file <- function(path) {
-  read.table(path, sep = "\t", header = TRUE)
-}
