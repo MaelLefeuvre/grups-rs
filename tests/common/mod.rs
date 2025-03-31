5766c8c773b6f667118e0605aee01b59fa5a66b2
MaelLefeuvre <lefeuvre.m.p@gmail.com>
2025-03-31 03:41:14 +0200

    Implement additional Integration tests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -4,4 +4,15 @@ pub use fixture::Fixture;
 
 #[cfg(test)]
 mod grups_runner;
-pub use grups_runner::test_grups_run;
+pub use grups_runner::GrupsRunnerBuilder;
+
+#[macro_export]
+macro_rules! validate_file {
+    ($ref_file:expr, $obtained_file:expr) => {
+        let want = include_bytes!($ref_file);
+        let got  = std::fs::read($obtained_file)
+            .unwrap_or_else(|_| panic!("Failed to open {:?}", $obtained_file));
+        assert_eq!(want.to_vec(), got)
+    };
+}
+
